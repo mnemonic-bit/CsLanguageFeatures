44b30bbed6e92dd97b57baef76bb107ac12c1f43
Marco Ruether <mruether@mnemonic-bit.com>
2024-10-23 14:48:09 +0200

    [master] 'Added comments'

--- CsLanguageFeatures/Version12/RefReadonlyParameters.cs
+++ CsLanguageFeatures/Version12/RefReadonlyParameters.cs
@@ -1,12 +1,14 @@
 ﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CsLanguageFeatures.Version12
 {
+    /// <summary>
+    /// This tests show that we can define ref parameters which cannot be changed
+    /// from inside the method itself. As opposed to an in-parameter type, which
+    /// can hold any expression instead of a single parameter name, the ref-readonly
+    /// signals to the caller that only a single variable is allowed here, and that
+    /// its value will not be changed inside the called method.
+    /// </summary>
     public class RefReadonlyParameters
     {
 
@@ -22,6 +24,29 @@ namespace CsLanguageFeatures.Version12
 
             // Assert
             result.Should().BeFalse();
+            value.Should().Be(TWELVE);
+        }
+
+        /// <summary>
+        /// This test is just for demonstration purposes to show that an in-parameter
+        /// can have a full-blown expression instead of being just a single parameter.
+        /// </summary>
+        [Fact]
+        public void InParameter_ShouldBehaveLikeRefReadonly_WhenPassedAnExpression()
+        {
+            bool InParameter(in int value)
+            {
+                return value == 0;
+            }
+
+            // Arrange
+            const int TWELVE = 12;
+            int value = TWELVE;
+
+            // Act
+            var result = InParameter(value + 1);
+
+            // Assert
             value.Should().Be(TWELVE);
         }
 
